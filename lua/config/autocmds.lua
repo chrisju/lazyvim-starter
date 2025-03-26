7e34d80d4925d215cc5615dc8fe7dc7111dd6084
chrisju <mchrisju@gmail.com>
2025-03-26 17:31:40 +0900

    disable auto comment

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- 创建一个自动命令组
+local group = vim.api.nvim_create_augroup("DisableAutoComment", { clear = true })
+
+-- 创建自动命令
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove("r")
+    vim.opt_local.formatoptions:remove("o")
+  end,
+  group = group,
+})
